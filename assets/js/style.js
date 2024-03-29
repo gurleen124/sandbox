3cbd68bd71188d73de974be926b0438f23cdcad9
Gurleen Kaur <gurleen.kaur@lpinfotech.com>
2024-03-29 19:16:50 +0530

    responsive

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-    $("li.demos").hover(function(){
-        $("div.demos").toggle();
-    })
+    $("li.menu").hover(function(){
+        $(this).children("ul").toggle();
+    });
+    $("li.subMenu").hover(function(){
+        $(this).children("ul").toggle();
+    });
+
+    $("input#check").click(function(){
+        $("h3.month").toggleClass("hidden");
+        $("h3.year").toggleClass("hidden");
+    });
+
+    $(window).scroll(function(){
+        $("a#scrollButton").removeClass("hidden");
+    });
+
+    // $("button#scrollButton").click(function(){
+    //     $("header").scrollTop(0);
+    // });
 });
